eb7d092f3c47b5c09b9561e735488e3de20b88ab
Rui Ferreira <rferreira@av.it.pt>
2015-06-13 08:07:10 +0000

    Add site info and rewrite to sbn.log

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ from sbn import SBNMiddleware
 import requests
 from urllib.parse import urlparse
 import logging
+import json
 
 app = Flask(__name__)
 key = b">\xed\x19\xa7Q\xe5\xa1\x00ZF\xe7X4(m\xb7\x92(\x01\x90\xb1\x16\x9b\x02\xb7\xa3-\xa87\xe8\x1b[\x1a\xef\xc16\xbc\xdf\x99\xac^\xcd\x0e\xfd,\xb5\x19\x02\x9ci\xf8>\xa5\x07\x85\xa8\xc8B\xe5\xb1\x1dt\xbfV\xab|\xdd,\xa7\xcdFf\xa5H\xc6\xf1\xa9\x84\xb0\xd1\x989\x1f\xac\xa5H\xed\xa5>x{g3\x17\xf5\x04*\t\x03\xb2_\x7f&\x12\x0f%\xa2\x0e\xa3\xc3Z\x10\xb0\xea\xeb\xb7\xbam>P2\xd7\x84>\xa1\xd5!\x02\xef\xd2\xc2\xe20\x88\xf7\xd0R\x89\x18U\xcc4\xd96W\xcd\xdb\x16\xb4\x98\xf5\xf0/\xc1\xad\xa8\xe1 \xdb0\xc8\x9c\xad\x00\xcc'!\xcb\x1e\xddv\x9b\x88}\xfbR\xe1\xef\xb7L\x93\x96\x0b\xb25'p\xc78\x10\x15\x8e[$\xcd\xf2D\xd4\x05y=\xa7u\xe1)E\x94@\xd2x\x06\xa2~\x88\x17\xc2\xf4\x86\xcc\xba^\xfb>\xaf\xc7\xe3R\x02mb'\x1a\xf8\xaf\xf4\x1b\xae\xe2l\x05\xd7\xb8n\xc99i\xc5\x118\xf2\x93\x01aT\xa4\xe6"
@@ -53,6 +54,7 @@ def proxystart():
     if not url_p.scheme:
         url_p = urlparse('http://'+url)
 
+    SBNLOG.info('--- %s' % url)
     return redirect(SBN.encode_url(url_p.geturl()))
 
 from lxml import html
@@ -75,7 +77,8 @@ def proxy(p):
 
         new_headers = {k:v for k,v in request.headers.items() if k in ('User-Agent', 'Connection', 'Dnt', 'Accept-Encoding', 'Accept-Language', 'Accept', 'Cookie')}
         req = requests.get(request.url, stream=True, headers=new_headers)
-        if req.headers['content-type'].startswith('text/html'):
+        rewrite = req.headers['content-type'].startswith('text/html')
+        if rewrite:
             doc = html.fromstring(req.text, base_url=request.url)
             doc.rewrite_links(proxy_repl, base_href=request.url)
             data = html.tostring(doc)
@@ -83,7 +86,9 @@ def proxy(p):
             resp =  Response(data, content_type=req.headers['content-type'])
         else:
             resp = Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
-        SBNLOG.info('{:s}/{:s} as {:s} {}'.format(request.environ.get('SBN_HTTP_HOST', ''), request.environ.get('SBN_PATH_INFO', ''), request.url, SBN.stats() ))
+        info = SBN.stats()
+        info['rewrite'] = rewrite
+        SBNLOG.info(json.dumps(info))
         return resp
     else:
         return root()
